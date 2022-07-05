b438f2d6653226338e1c1b8227e43ec50125e347
piotrostr <piotr.ostrowski@aol.com>
2022-07-05 20:44:33 +0200

    add minimal frontend for the example

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,28 @@
 import { FunctionalComponent, h } from 'preact';
+import { IItem, Item } from './item';
+
+const items = [
+  { name: 'First Item', price: 1 },
+  { name: 'Second Item', price: 2 },
+  { name: 'Third Item', price: 3 },
+  { name: 'Fourth Item', price: 4 },
+];
 
 const App: FunctionalComponent = () => {
-  return <div id="preact_root">hello world!</div>;
+  function onPressCheckout(item: IItem) {
+    alert(`checking out with ${item.name}`);
+  }
+
+  return (
+    <div className="w-full h-full">
+      <h1 className="text-3xl mb-5">gopay example</h1>
+      <div className="flex justify-between p-7 max-h-96">
+        {items.map((item) => (
+          <Item item={item} onPressCheckout={onPressCheckout} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/item.tsx
@@ -0,0 +1,31 @@
+import { h } from 'preact';
+
+export interface IItem {
+  name: string;
+  price: number;
+}
+
+interface Props {
+  item: IItem;
+  onPressCheckout: (item: IItem) => void;
+}
+
+export const Item = ({ item, onPressCheckout }: Props) => {
+  return (
+    <div className="bg-gray-100 w-64 rounded-md">
+      <div className="flex flex-row p-2">
+        <div className="w-12 h-12 rounded-md bg-orange-300 mr-3" />
+        <div>
+          <h1 className="text-lg">{item.name}</h1>
+          <p className="text-gray-400">${item.price}.00</p>
+        </div>
+      </div>
+      <div
+        className="w-full h-10 flex justify-center items-center bg-blue-100 rounded-b-md cursor-pointer"
+        onClick={() => onPressCheckout(item)}
+      >
+        Checkout
+      </div>
+    </div>
+  );
+};
