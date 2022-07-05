977184d0a1a00e4bbe32762ee5827342fad58477
piotrostr <piotr.ostrowski@aol.com>
2022-07-05 20:46:26 +0200

    feat: minimal mobile scaling

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -14,9 +14,9 @@ const App: FunctionalComponent = () => {
   }
 
   return (
-    <div className="w-full h-full">
-      <h1 className="text-3xl mb-5">gopay example</h1>
-      <div className="flex justify-between p-7 max-h-96">
+    <div className="w-full h-full p-10">
+      <h1 className="text-3xl mb-5 p-5">gopay example</h1>
+      <div className="flex flex-col justify-between p-7 max-h-96 space-y-5 md:flex-row md:space-y-0">
         {items.map((item) => (
           <Item item={item} onPressCheckout={onPressCheckout} />
         ))}
